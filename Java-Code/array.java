f86ffb1cf30073707216d819a1ec6991d08d7af6
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-06 14:02:46 +0530

    Add files via upload

---
+++ Java-Code/array.java
@@ -0,0 +1,43 @@
+/*
+class Array
+{
+	public static void main (String[] args)
+	{
+		int[] a = new int[3];
+		System.out.println(a.length);
+		System.out.println(a);
+		System.out.println(a[0]);
+	}
+}
+*/
+/*
+class Array
+{
+	public static void main(String[] args)
+	{
+		int[] [] a= new int [2][3];
+		System.out.println(a);
+		System.out.println(a[0]);
+		System.out.println(a[0][0]);
+	}
+}*/
+	
+	
+class Array
+{
+	public static void main(String[] args)
+	{
+		int [] [] [] a = {{{10,20,30},{40,50}},{{60},{70,80},{90,100,110}}};
+		System.out.println(a[1][2][1]);
+	}
+}
+
+
+
+
+
+
+
+
+
+
