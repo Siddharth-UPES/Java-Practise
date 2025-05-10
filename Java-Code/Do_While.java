97de03b9850e9c934f24e950a9c35e0b725f23f0
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-10 21:33:38 +0530

    Add files via upload

---
+++ Java-Code/Do_While.java
@@ -0,0 +1,13 @@
+class Do_Whil
+{
+	public static void main(String[] args)
+	{
+		int a=10,b=20;
+		do
+		{
+			System.out.println("hello");
+		}
+			while(a<b);
+			System.out.println("hii");
+	}
+}
