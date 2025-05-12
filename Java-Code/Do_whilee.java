51918279fb5d75eadd8a0e12a6db08417cf4f141
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-13 00:56:13 +0530

    Add files via upload

---
+++ Java-Code/Do_whilee.java
@@ -0,0 +1,13 @@
+class Do_whilee
+{
+	public static void main(String args[])
+	{
+		final int a=10, b=20;
+		do
+		{
+			System.out.println("Hello");
+		}
+		while (a>b);
+		System.out.println("false condtions thats why i am printing this msg");
+	}
+}
