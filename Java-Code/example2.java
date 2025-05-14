9d30b7d4845db248b78419507e2b11c687525296
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-14 23:17:18 +0530

    Add files via upload

---
+++ Java-Code/example2.java
@@ -0,0 +1,16 @@
+class Test
+{
+	public static void main(String[] args)
+	{
+		boolean b=false;
+		if(b=true)
+		{
+			System.out.println("hello");
+		}
+		else
+		{
+			System.out.println("bye");
+		}
+	}
+
+}
