a376d36bd722ddb3de31787650b2813746a4a4d7
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-15 16:39:23 +0530

    Add files via upload

---
+++ Java-Code/ExampleIF.java
@@ -0,0 +1,15 @@
+public class ExampleIF
+{
+	public static void main(String[] args)
+	{
+		int x=0;
+		if(x==10)
+		{
+			System.out.println("Hello");
+		}
+		else
+		{
+			System.out.println("Hi");
+		}
+	}
+}
