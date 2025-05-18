9f8aad0bb58f74c34facdeab04959b8f5c8ad259
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-19 02:35:43 +0530

    Add files via upload

---
+++ Java-Code/ExampleSwitch4.java
@@ -0,0 +1,17 @@
+class  ExampleSwitch
+{
+	public static void main(String[] args)
+	{
+		byte b=10;
+		switch(b+1)
+		{
+			case 10:
+				System.out.println("10");
+			case 100:
+				System.out.println("100");
+			case 1000:
+				System.out.println("1000");
+		}
+	}
+	
+}
