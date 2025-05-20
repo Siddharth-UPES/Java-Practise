7b126aa122faa15c12ea86007b0c61d11f380124
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-20 23:59:53 +0530

    Add files via upload

---
+++ Java-Code/ExampleSwitch5.java
@@ -0,0 +1,22 @@
+class ExampleSwitch
+{
+	public static void main(String[] args)
+	{
+		int x=0;
+		switch(x)
+		{
+			
+			case 0:
+				System.out.println("0");
+			case 1:
+				System.out.println("1");
+			default:
+				System.out.println("default");
+			case 2:
+				System.out.println("2");
+			case 3:
+				System.out.println("3");
+			
+		}
+	}
+}
