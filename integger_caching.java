ba54833275fe6f50116817418308244ebd3f6479
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-28 00:10:23 +0530

    Add files via upload

---
+++ integger_caching.java
@@ -0,0 +1,18 @@
+class Test
+{
+	public static void main(String... args)
+	{
+		Integer num1=200;
+		Integer num2=200;
+		
+		if(num1==num2)
+		{
+			System.out.println("both numbers are same");
+		}
+
+		else
+		{	
+			System.out.println("both number are not same");
+		}
+	}
+}
