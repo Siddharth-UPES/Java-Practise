13e5feb33a6823def073d02b023dcf5347e39907
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-29 17:01:05 +0530

    Add files via upload

---
+++ Java-Code/j.java
@@ -0,0 +1,20 @@
+class myThread extends Thread 
+{	
+	public void start()
+	{
+		System.out .println("start method ");
+	}
+	public void run()
+	{
+		System.out.println("run method");
+	}
+}
+class Test
+{
+	public static void main(String... args)
+	{
+		myThread t= new myThread();
+		t.start();
+		System.out.println("main method");
+	}
+}
