61283d845297c2a3845b82993c6a5b0348c02d62
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-30 02:06:00 +0530

    Add files via upload

---
+++ Java-Code/length.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class length
+{
+	public static void main(String... args)
+	{
+		String s="Siddharth";
+		System.out.println(s.length());
+		//System.out.println(s.length);
+	}
+}
