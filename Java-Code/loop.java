d3545ad88859c34931365a3f3b98862598ee7a08
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-05-31 13:21:19 +0530

    Add files via upload

---
+++ Java-Code/loop.java
@@ -0,0 +1,11 @@
+class loop
+{
+	public static void main(String[] args)
+	{
+		int i=0;
+		for(System.out.println("hello u are working");i<5;i++)
+		{
+			System.out.println("yes boss, we are working for u ");
+		}
+	}
+}
