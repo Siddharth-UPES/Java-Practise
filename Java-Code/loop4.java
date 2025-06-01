16235a1841d83cc8f82015e884540770a043b981
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-06-01 19:12:24 +0530

    Add files via upload

---
+++ Java-Code/loop4.java
@@ -0,0 +1,25 @@
+/*
+class loop4
+{
+	public static void main(String[] agrs)
+	{
+		int[] a={10,20,30,40,50,60,70,80,90};
+		for(int i=0;i<a.length;i++)
+		{
+			System.out.println(a[i]);
+		}
+	}
+}
+*/
+
+class loop4
+{
+	public static void main(String[] args)
+	{
+		int[] a={10,20,30,40,50,60,70};
+		for(int x:a)
+		{
+			System.out.println(x);
+		}
+	}
+}
