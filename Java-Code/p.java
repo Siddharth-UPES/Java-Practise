57362d10956351262e9c1a2d4a180edbe0cf7bac
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-06-02 22:02:39 +0530

    Add files via upload

---
+++ Java-Code/p.java
@@ -0,0 +1,18 @@
+class P
+{
+	public void property()
+	{
+		System.out.println("ca$h+lanD+Gold");
+	}	
+	public final void marry()
+	{
+		System.out.println("Kajal");
+	}
+}
+class c extends P
+{
+	public void marry()
+	{
+		System.out.println("3sha/9tata/Rani");
+	}
+}
