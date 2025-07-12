2990942f0833ac35f500becfabfd32c7f1fef0e5
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-07-12 22:06:31 +0530

    Add files via upload

---
+++ Java-Code/Loop.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+public class Loop {
+    public static void main(String[] args)
+    {
+//        int num=1;
+//        while (num<=1000)
+//        {
+//            System.out.println(num);
+//            num=num+1;
+//        }
+
+
+        int count=500;
+        while (count>=200)
+        {
+            System.out.println(count);
+            count-=1;
+        }
+
+
+
+//        Scanner s=new Scanner(System.in);
+//        int i=0;
+//        while(i<5){
+//            int input=s.nextInt();
+//            System.out.println("number is "+input);
+//            i++;
+//        }
+    }
+}
---
+++ Java-Code/function.java
@@ -0,0 +1,14 @@
+public class function {
+    public static void main(String[] args) {
+        System.out.println("IN main method");
+
+        greeting();// call the funtion
+        System.out.println("Main calling method");
+    }
+
+
+    public static void greeting() {
+        System.out.println("Good Morning call the function");
+
+    }
+}
