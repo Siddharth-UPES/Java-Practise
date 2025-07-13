5b1a5fb4298a87d831ea9d111eb4eead22ce9f32
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-07-14 00:04:27 +0530

    Add files via upload

--- Java-Code/Main.java
+++ Java-Code/Main.java
@@ -1,9 +1,29 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello and welcome!");
+
+/*
+class main{
+    public static void main(String[] args)
+    {
+        int a=4;
+        int b=5;
+        System.out.println(--a+"\n"+b++);
+        System.out.println(--a*b++);
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+
+}
+*/
+
+class main{
+    public static void main(String[] args){
+        int x=5;
+        System.out.println(-5 & 6);
+        System.out.println(-5 | -6);
+        System.out.println(~9);
+        System.out.println(4<<10);
+        System.out.println(4>>2);
+        System.out.println(-4>>>2);
+        System.out.println(++x);
     }
 }
