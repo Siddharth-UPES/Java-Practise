feddefc6754f34437a82e5f2f49107883c9c8b20
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-07-14 00:02:24 +0530

    Add files via upload

--- Java-Code/Main.java
+++ Java-Code/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-       Scanner input=new
-
+        System.out.println("*\n* *\n* * *\n* * * * \n* * * * *\n");
+        System.out.println("*****\n****\n***\n**\n*");
     }
 }
