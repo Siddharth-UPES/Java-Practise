68c3fd44a2cf36993f858d7b335304214997d68e
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-07-14 00:05:11 +0530

    Add files via upload

--- Java-Code/Main.java
+++ Java-Code/Main.java
@@ -1,29 +1,8 @@
+import java.util.Scanner;
+ public class User_input {
+     public static void main(String[] args) {
+         Scanner input = new Scanner(System.in);
 
-/*
-class main{
-    public static void main(String[] args)
-    {
-        int a=4;
-        int b=5;
-        System.out.println(--a+"\n"+b++);
-        System.out.println(--a*b++);
-        System.out.println(a);
-        System.out.println(b);
+     }
 
-    }
-
-}
-*/
-
-class main{
-    public static void main(String[] args){
-        int x=5;
-        System.out.println(-5 & 6);
-        System.out.println(-5 | -6);
-        System.out.println(~9);
-        System.out.println(4<<10);
-        System.out.println(4>>2);
-        System.out.println(-4>>>2);
-        System.out.println(++x);
-    }
 }
