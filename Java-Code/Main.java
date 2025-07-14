3c38ee5498240fa0d327b2233b68971575abfbc2
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-07-14 22:17:38 +0530

    Add files via upload

--- Java-Code/Main.java
+++ Java-Code/Main.java
@@ -1,8 +1,9 @@
 import java.util.Scanner;
- public class User_input {
-     public static void main(String[] args) {
-         Scanner input = new Scanner(System.in);
-
-     }
-
+public class Main {
+    public static void main(String[] args) {
+    Scanner input=new Scanner(System.in);
+    System.out.println("Enter your name first");
+    String name=input.nextLine();
+    System.out.println("Welcome "+name+" to the KG Coding");
+    }
 }
