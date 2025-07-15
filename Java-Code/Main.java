ba4c51cbc442282ea35f7fe38d4fd2695163fca9
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-07-15 21:43:37 +0530

    Add files via upload

--- Java-Code/Main.java
+++ Java-Code/Main.java
@@ -1,9 +1,23 @@
 import java.util.Scanner;
-public class Main {
+
+class Airthmatic {
     public static void main(String[] args) {
-    Scanner input=new Scanner(System.in);
-    System.out.println("Enter your name first");
-    String name=input.nextLine();
-    System.out.println("Welcome "+name+" to the KG Coding");
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter First number:");
+        int first=input.nextInt();
+        System.out.println("Enter Second number");
+        int second=input.nextInt();
+
+        int add=first+second;
+        int sub=first-second;
+        int mul=first*second;
+        int div=first/second;
+        int mod=first%second;
+
+        System.out.println("Addition is :" + add);
+        System.out.println("Subtraction is :" + sub);
+        System.out.println("Multiplication is :" + mul);
+        System.out.println("Division is :" + div);
+        System.out.println("Moduluson is :" + mod);
     }
 }
