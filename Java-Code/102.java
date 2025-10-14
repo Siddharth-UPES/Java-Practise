77708e419ae4fca97e24eb5a7ca0899b860446c7
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-10-14 23:27:14 +0530

    Add files via upload

---
+++ Java-Code/102.java
@@ -0,0 +1,40 @@
+/*
+class Test
+{
+	public static void main(String[] args)
+	{
+		int one='A'/'A';
+		String s1="..........";
+		for(int i=one ; i<=(s1.length()*s1.length());i++)
+		{
+			System.out.println(i);
+		}
+	}
+
+}
+*/
+
+/*
+class Test
+{
+	public static void main(String[] args)
+	{
+		for(int i=1;i<=100;i++)
+		{
+			System.out.println(i);
+		}
+	}
+}
+*/
+
+class Test
+{
+	public static void main(String... args)
+	{
+		int one='b'/'b';
+		for(int i=one;i<='d';i+=one)
+		{
+			System.out.println(i);
+		}
+	}
+}
