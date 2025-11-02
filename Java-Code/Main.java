547c0463f3596a026f4f9499d522466e780aee78
Siddharth-Kumar <Siddharth16229@gmail.com>
2025-11-02 23:23:26 +0530

    Add files via upload

--- Java-Code/Main.java
+++ Java-Code/Main.java
@@ -1,55 +1,15 @@
-//import java.util.Scanner;
-//
-//public class Main {
-//    public static void main(String[] args) {
-//        Scanner sc=new Scanner(System.in);
-//        System.out.println("Welcome to multiplication of table :");
-//        System.out.print("Enter the number for table : ");
-//        int num=sc.nextInt();
-//        for (int i=1;i<=10;i++){
-//            System.out.println(num+" x "+i+" = "+(num*i));
-//        }
-//    }
-//}
-
-//import java.util.Scanner;
-//
-//class SumOdd {
-//    public static void main(String[] args) {
-//        Scanner sc = new Scanner(System.in);
-//        System.out.print("Enter n: ");
-//        int n = sc.nextInt();
-//
-//        int sum = 0;
-//        for (int i = 1; i <= n; i++) {
-//            int odd = 2 * i - 1;  // ith odd number
-//            sum += odd;
-//        }
-//
-//        System.out.println("Sum of first " + n + " odd numbers = " + sum);
-//    }
-//}
-
-import java.util.Scanner;
-
-class Main{
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        System.out.print("ENter the number for the factorial : ");
-        int num=sc.nextInt();
-        long faactorial_no=faactorial(num);
-        System.out.println("The factorial of number is : "+faactorial_no);
-    }
-    public static long faactorial(int num){
-        if(num<2) {
-            return 1;
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
+
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-        long fact=1;
-        int i=2;
-        while(i<=num){
-            fact=fact*i;
-            i++;
-        }
-        return fact;
     }
 }
